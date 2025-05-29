ea6ec744a1380f6160a97dfac3b71e558290ce18
TanishaDatman <tanisha@datman.je>
2025-05-29 12:30:38 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <>
+  
+      <p className="bg-blue-700">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
