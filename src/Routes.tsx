b3ab309a4b45c757b04fd1284ce8793f4c2a46ec
TanishaDatman <tanisha@datman.je>
2025-06-06 17:39:13 +0530

    Grafana Embedded

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Chart2 from './components/Graphs/Chart2';
 import DashboardChart from './components/Graphs/DashboardChart';
+import GraphGrafana from './components/Grafana/GraphGrafana';
 // import { isAuthenticated } from './utils/auth';
 
 // const PrivateRoute = ({ children }: { children: React.ReactNode }) => {
@@ -14,6 +15,8 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<DashboardChart />} />
         <Route path="/chart" element={<Chart2 />} />
+        <Route path="/grafana-chart" element={<GraphGrafana/>} />
+
 
       </Routes>
     </Router>
---
+++ src/components/Grafana/GraphGrafana.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function GraphGrafana() {
+  return (
+<iframe src="http://grafana.datman.tools/dashboard/snapshot/7IEQcNTKOjI0XxtwpUY2W2y4Lpuk9tls"
+className='h-screen w-full'
+//  width="1000" 
+//  height="2000" 
+ frameBorder="0" />
+ )
+}
+
+export default GraphGrafana
+
+
+
+
